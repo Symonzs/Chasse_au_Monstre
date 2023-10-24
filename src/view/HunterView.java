f9386049be2c0e18aee8e888a859effead0effc7
charles.coude.etu <charles.coude.etu@univ-lille.fr>
2023-10-24 12:05:19 +0200

    ajout du squellette de View v1

--- src/view/HunterView.java
+++ src/view/HunterView.java
@@ -1,9 +1,35 @@
 package view;
 
-public class HunterView {
-    
-    public void showView(){
-        
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import r304.main.java.utils.Observer;
+import r304.main.java.utils.Subject;
+
+public class HunterView extends Stage implements Observer {
+
+    public void showView() {
+
+    }
+
+    public void start(Stage primaryStage) {
+        VBox vBox = new VBox();
+        Scene scene = new Scene(vBox, 1080, 780);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /* Overide Methode */
+    @Override
+    public void update(Subject arg0) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void update(Subject arg0, Object arg1) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
     }
 
 }
