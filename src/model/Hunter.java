ab236d8db350c3b062a8611009d8b2773273d5b9
Raphaël KIECKEN <raphael.kiecken.etu@univ-lille.fr>
2023-11-02 13:09:22 +0100

    Add javadoc comment on all the modal's code

--- src/model/Hunter.java
+++ src/model/Hunter.java
@@ -8,25 +8,75 @@ import fr.univlille.iutinfo.r304.utils.Observer;
 import fr.univlille.iutinfo.r304.utils.Subject;
 import fr.univlille.iutinfo.cam.player.perception.ICoordinate;
 
+/**
+ * Classe représentant le chasseur et les informations qu'il possède
+ * 
+ * @see CellEvent
+ * @see Coordinate
+ */
 public class Hunter implements Observer {
 
+    // Tableau de booléens représentant les murs du labyrinte connus par le chasseur
     private boolean[][] knowWall;
+    // Coordonnées des monstres connus par le chasseur
     private Map<Integer, ICoordinate> knowMonsterCoords;
-    private ICoordinate hunterCoord;
 
+    /**
+     * Constructeur de la classe Hunter
+     * Instancie les différents attributs de la classe
+     * - knowWall -> En créant un tableau de booléens de la taille du labyrinthe
+     * - knowMonsterCoords -> En créant une nouvelle TreeMap
+     * 
+     * @param rows Le nombre de lignes du labyrinthe
+     * @param cols Le nombre de colonnes du labyrinthe
+     */
     public Hunter(Integer rows, Integer cols) {
         this.knowWall = new boolean[rows][cols];
         this.knowMonsterCoords = new TreeMap<>();
     }
 
-    public Integer getMonsterTurn(ICoordinate coord) {
-        Integer turn = 0;
-        for (Map.Entry<Integer, ICoordinate> entry : knowMonsterCoords.entrySet()) {
+    /**
+     * Méthode permettant d'ajouter les coordonnées d'un monstre dans la liste des
+     * coordonées connus par le chasseur
+     * 
+     * @param monster Les coordonnées du monstre
+     */
+    public void addKnowMonsterCoords(ICoordinate monster) {
+        this.knowMonsterCoords.put(Maze.turn, monster);
+    }
+
+    /**
+     * Méthode permettant de récupérer les murs connus par le chasseur
+     * 
+     * @return Les murs connus par le chasseur
+     */
+    public boolean[][] getKnowWall() {
+        return this.knowWall;
+    }
+
+    /**
+     * Méthode permettant de récupérer les coordonnées des monstres connus par le
+     * chasseur
+     * 
+     * @return Les coordonnées des monstres connus par le chasseur
+     */
+    public Map<Integer, ICoordinate> getKnowMonsterCoords() {
+        return this.knowMonsterCoords;
+    }
+
+    /**
+     * Méthode permettant de récupérer le tour durant lequel le monstre est passé
+     * sur la case donnée en paramètre
+     * 
+     * @param coord Les coordonnées de la case
+     */
+    public Integer getMonsterTurnFromCoordinate(ICoordinate coord) {
+        for (Map.Entry<Integer, ICoordinate> entry : this.knowMonsterCoords.entrySet()) {
             if (entry.getValue().equals(coord)) {
-                turn = entry.getKey();
+                return entry.getKey();
             }
         }
-        return turn;
+        return null;
     }
 
     @Override
@@ -37,14 +87,12 @@ public class Hunter implements Observer {
             for (int j = 0; j < this.knowWall[0].length; j++) {
                 ICoordinate coord = new Coordinate(i, j);
                 if (this.knowMonsterCoords.containsValue(coord)) {
-                    Integer monsterTurn = this.getMonsterTurn(coord);
+                    Integer monsterTurn = this.getMonsterTurnFromCoordinate(coord);
                     if (Maze.turn.equals(monsterTurn)) {
                         sb.append("M ");
                     } else if (monsterTurn != null) {
                         sb.append(monsterTurn + " ");
                     }
-                } else if (coord.equals(this.hunterCoord)) {
-                    sb.append("H ");
                 } else if (this.knowWall[i][j]) {
                     sb.append("W ");
                 } else {
@@ -56,15 +104,26 @@ public class Hunter implements Observer {
         return sb.toString();
     }
 
-    public void addKnowMonsterCoords(ICoordinate monster) {
-        this.knowMonsterCoords.put(Maze.turn, monster);
-    }
-
     @Override
     public void update(Subject arg0) {
-        // Method neverUsed
+        // Methode non utilisée
     }
 
+    /**
+     * Méthode permettant de mettre à jour les coordonnées connus par le chasseur
+     * Vérifie si l'objet passé en paramètre est une instance de CellEvent
+     * Si c'est le cas, vérifie l'état de la case et met à jour les coordonnées
+     * connus par le chasseur
+     * Si l'état de la case est MONSTER, ajoute les coordonnées du monstre dans la
+     * liste des coordonnées connus par le chasseur
+     * Si l'état de la case est WALL, met à jour la case dans le tableau de booléens
+     * avec la valeur true
+     * Sinon met à jour la case dans le tableau de booléens avec la valeur false
+     * 
+     * @param arg0 Le labyrinth
+     * @param arg1 Le CellEvent contenant les informations sur la case touchée
+     * 
+     */
     @Override
     public void update(Subject arg0, Object arg1) {
         if (CellEvent.class == arg1.getClass()) {
--- src/model/Maze.java
+++ src/model/Maze.java
@@ -9,37 +9,75 @@ import java.util.TreeMap;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Logger;
 
 import fr.univlille.iutinfo.cam.player.perception.ICoordinate;
 import fr.univlille.iutinfo.cam.player.perception.ICellEvent.CellInfo;
 import fr.univlille.iutinfo.r304.utils.Observer;
 import fr.univlille.iutinfo.r304.utils.Subject;
 
+/**
+ * Classe représentant le labyrinthe
+ * 
+ * @see CellEvent
+ * @see Coordinate
+ * @author Simon HAYART, Raphael KIECKEN, Charles COUDE, Hugo
+ */
 public class Maze extends Subject {
 
+    // Tableau de booléens représentant les murs du labyrinte
     private boolean[][] wall;
+    // Map représentant les coordonnées des monstres
     private Map<Integer, ICoordinate> monster;
-    private ICoordinate hunter;
+    // Map représentant les coordonnées du chasseur
+    private Map<Integer, ICoordinate> hunter;
+    // Coordonnées de la sortie
     private ICoordinate exit;
+    // Tour actuel
     public static Integer turn = 1;
 
-    Logger log = Logger.getLogger(getClass().getName());
-
+    /**
+     * Constructeur de la classe Maze
+     * 
+     * @param fileName Nom du fichier contenant le labyrinthe
+     */
     public Maze(String fileName) {
         loadMaze(fileName);
     }
 
+    /**
+     * Constructeur de la classe Maze
+     * Créer un nom de fichier à partir du nombre de lignes et de colonnes sous la
+     * forme "'nbRows'x'nbCols'.csv"
+     * 
+     * @param nbRows Nombre de lignes du labyrinthe
+     * @param nbCols Nombre de colonnes du labyrinthe
+     */
     public Maze(Integer nbRows, Integer nbCols) {
         loadMaze(String.format("%dx%d.csv", nbRows, nbCols));
     }
 
+    /**
+     * Charge le labyrinthe à partir d'un fichier
+     * Instancie les attributs de la classe (wall, monster, hunter, exit)
+     * Selon le caractère lu dans le fichier :
+     * - Rempli le tableau de booléens représentant les murs du labyrinthe
+     * - Ajoute les coordonnées des monstres dans la map
+     * - Met à jour les coordonnées de la sortie
+     * 
+     * Le fichier doit être au format CSV
+     * 
+     * Si un caractère non reconnu est lu, une exception est levée
+     * 
+     * @param fileName Nom du fichier contenant le labyrinthe
+     */
     public void loadMaze(String fileName) {
         try {
             List<String> lines = Files.readAllLines(importFileWithPath(fileName).toPath(), StandardCharsets.UTF_8);
             Integer nbRows = lines.size();
             Integer nbCols = lines.get(0).split(",").length;
             this.wall = new boolean[nbRows][nbCols];
+            this.monster = new TreeMap<>();
+            this.hunter = new TreeMap<>();
             for (int i = 0; i < lines.size(); i++) {
                 String line = lines.get(i);
                 String[] cols = line.split(",");
@@ -52,7 +90,6 @@ public class Maze extends Subject {
                             this.wall[i][j] = false;
                         }
                         case "M" -> {
-                            this.monster = new TreeMap<>();
                             this.monster.put(turn, new Coordinate(i, j));
                             this.wall[i][j] = false;
                         }
@@ -61,10 +98,17 @@ public class Maze extends Subject {
                 }
             }
         } catch (IOException | InputMismatchException | NullPointerException e) {
-            log.warning(e.getMessage());
+            System.err.println(e.getMessage());
         }
     }
 
+    /**
+     * Importe un fichier à partir de son nom
+     * 
+     * @param fileName Nom du fichier à importer
+     * @return Fichier importé
+     * @throws FileNotFoundException Si le fichier n'est pas trouvé
+     */
     public File importFile(String fileName) throws FileNotFoundException {
         File csv = FileFinder.find(fileName);
         if (csv == null) {
@@ -73,10 +117,24 @@ public class Maze extends Subject {
         return csv;
     }
 
+    /**
+     * Importe un fichier à partir de son chemin absolu
+     * 
+     * @param pathName Chemin du fichier à importer
+     * @return Fichier importé
+     * @throws NullPointerException Si le chemin est null
+     */
     public File importFileWithPath(String pathName) throws NullPointerException {
         return new File(pathName);
     }
 
+    /**
+     * Met à jour la case du labyrinthe ciclée par le CellEvent
+     * Si l'état du CellEvent est HUNTER, appel la méthode cellUpdateHunter
+     * Si l'état du CellEvent est MONSTER, appel la méthode cellUpdateMonster
+     * 
+     * @param eventRequest CellEvent à traiter
+     */
     public void cellUpdate(CellEvent eventRequest) {
         if (CellInfo.HUNTER.equals(eventRequest.getState())) {
             cellUpdateHunter(eventRequest.getCoord(), eventRequest.getTurn());
@@ -85,6 +143,16 @@ public class Maze extends Subject {
         }
     }
 
+    /**
+     * Met à jour la case du labyrinthe ciclée par le CellEvent
+     * Ajoute les coordonnées du monstre dans la map
+     * Si après l'ajout des coordonnées du monstre, le monstre est sur la sortie,
+     * appel la méthode end
+     * Sinon, appel la méthode notifyObserver
+     * 
+     * @param eventCoord Nouvelle coordonnées du monstre
+     * @param eventTurn  Tour du déplacement du monstre
+     */
     private void cellUpdateMonster(ICoordinate eventCoord, Integer eventTurn) {
         this.monster.put(eventTurn, eventCoord);
         if (this.monsterAtExit()) {
@@ -94,10 +162,35 @@ public class Maze extends Subject {
         }
     }
 
+    /**
+     * Met à jour la case du labyrinthe ciclée par le CellEvent
+     * Ajoute les coordonnées du chasseur dans la map
+     * Si le monstre a déjà été sur cette case, on vérifie si le monstre y est
+     * toujours
+     * Si le monstre y est toujours, appel la méthode end
+     * Sinon, on stocke dans eventHunter qui est un CellEvent(Coordonnées, Tour,
+     * Etat)
+     * - Les coordonnées de la case
+     * - Le tour du tir
+     * - L'état de la case (MONSTER)
+     * Si le monstre n'a jamais été sur cette case, on stocke dans eventHunter qui
+     * est un CellEvent(Coordonnées, Etat)
+     * - Les coordonnées de la case
+     * - L'état de la case (EMPTY ou WALL selon si la case est vide ou non)
+     * 
+     * On stocke également dans eventMonster qui est un CellEvent(Coordonnées, Etat)
+     * - Les coordonnées de la case
+     * - L'état de la case (HUNTER)
+     * 
+     * Enfin, on appel la méthode notifyObserver
+     * 
+     * @param eventCoord Nouvelle coordonnées du chasseur
+     * @param eventTurn  Tour du déplacement du chasseur
+     */
     private void cellUpdateHunter(ICoordinate eventCoord, Integer eventTurn) {
         CellEvent eventHunter = null;
         CellEvent eventMonster = null;
-        this.hunter = eventCoord;
+        this.hunter.put(eventTurn, eventCoord);
         if (this.monsterWasHere(eventCoord)) {
             if (this.monsterIsHere(eventCoord)) {
                 this.end(CellInfo.HUNTER);
@@ -115,50 +208,139 @@ public class Maze extends Subject {
         this.notifyObserver(eventHunter, eventMonster);
     }
 
+    /**
+     * Vérifie si le monstre est déjà passé sur la case
+     * 
+     * @param eventCoord
+     * @return true si le monstre est déjà passé sur la case, false sinon
+     */
     public boolean monsterWasHere(ICoordinate eventCoord) {
         return this.monster.containsValue(eventCoord);
     }
 
+    /**
+     * Vérifie si le monstre est sur la case
+     * 
+     * @param eventCoord
+     * @return true si le monstre est sur la case, false sinon
+     */
     public boolean monsterIsHere(ICoordinate eventCoord) {
         return this.monster.get(Maze.turn).equals(eventCoord);
     }
 
+    /**
+     * Vérifie si la case est un mur
+     * 
+     * @param eventCoord
+     * @return true si la case est un mur, false sinon
+     */
     public boolean cellIsWall(ICoordinate eventCoord) {
         return this.wall[eventCoord.getRow()][eventCoord.getCol()];
     }
 
+    /**
+     * Vérifie si le monstre est sur la sortie
+     * 
+     * @return true si le monstre est sur la sortie, false sinon
+     */
     public boolean monsterAtExit() {
         return exit.equals(this.monster.get(Maze.turn));
     }
 
+    /**
+     * Met fin à la partie
+     * Pas encore implémenté
+     * 
+     * @param victoryInfo Qui du monstre ou du chasseur a gagné
+     */
     public void end(CellInfo victoryInfo) {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Incrémente le compteur de tour
+     */
     public static void incrementTurn() {
         Maze.turn++;
     }
 
+    /**
+     * Réinitialise le compteur de tour
+     */
     public static void resetTurn() {
         Maze.turn = 1;
     }
 
+    /**
+     * Retourne le tableau de booléens représentant les murs du labyrinthe
+     * 
+     * @return Tableau de booléens représentant les murs du labyrinthe
+     */
     public boolean[][] getWall() {
         return this.wall;
     }
 
+    /**
+     * Retourne les coordonnées de la sortie
+     * 
+     * @return Coordonnées de la sortie
+     */
     public ICoordinate getExit() {
         return this.exit;
     }
 
+    /**
+     * Retourne la map des coordonnées du monstre
+     * 
+     * @return Map des coordonnées du monstre
+     */
     public Map<Integer, ICoordinate> getMonster() {
         return this.monster;
     }
 
-    public ICoordinate getHunter() {
+    /**
+     * Retourne la map des coordonnées du chasseur
+     * 
+     * @return Map des coordonnées du chasseur
+     */
+    public Map<Integer, ICoordinate> getHunter() {
         return this.hunter;
     }
 
+    /**
+     * Retourne les coordonnées du monstre au tour actuel si elles existent, sinon
+     * retourne les coordonnées du monstre au tour précédent
+     * 
+     * @return Coordonnées du monstre au tour actuel ou au tour précédent
+     */
+    public ICoordinate getLastMonsterCoordinate() {
+        ICoordinate lastMonsterCoord = this.monster.get(Maze.turn);
+        if (lastMonsterCoord == null) {
+            lastMonsterCoord = this.monster.get(Maze.turn - 1);
+        }
+        return lastMonsterCoord;
+    }
+
+    /**
+     * Retourne les coordonnées du chasseur au tour actuel si elles existent, sinon
+     * retourne les coordonnées du chasseur au tour précédent
+     * 
+     * @return Coordonnées du chasseur au tour actuel ou au tour précédent
+     */
+    public ICoordinate getLastHunterCoordinate() {
+        ICoordinate lastHunterCoord = this.hunter.get(Maze.turn);
+        if (lastHunterCoord == null) {
+            lastHunterCoord = this.hunter.get(Maze.turn - 1);
+        }
+        return lastHunterCoord;
+    }
+
+    /**
+     * Notifie les observateurs de la classe
+     * Si l'observateur est un chasseur et que ses données ne sont pas null,
+     * appel la méthode update de la classe Hunter
+     * Si l'observateur est un monstre, appel la méthode update de la classe Monster
+     */
     public void notifyObserver(Object hunterData, Object monsterData) {
         for (Observer observer : this.attached) {
             if (Hunter.class == observer.getClass()) {
