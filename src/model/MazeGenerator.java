c0f896f18ea44be129cd3c695b9ebeed18a7d2e1
Raphaël Kiecken <raphael.kiecken.etu@univ-lille.fr>
2023-12-14 16:35:03 +0100

    debut generation labyrinthe de bz

--- src/model/MazeGenerator.java
+++ src/model/MazeGenerator.java
@@ -4,67 +4,146 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.Stack;
 import java.util.TreeMap;
 
 import fr.univlille.iutinfo.cam.player.perception.ICoordinate;
 
 public class MazeGenerator {
 
-    private final Random random = new Random();
-
-    private List<ICoordinate> path;
-    private List<ICoordinate> visited;
-
     private boolean[][] wall;
     private Map<Integer, ICoordinate> monster;
     private Map<Integer, ICoordinate> hunter;
     private ICoordinate exit;
 
+    private Random random = new Random();
+
     public MazeGenerator(Integer nbRows, Integer nbCols, Integer wallPercent) {
         wall = new boolean[nbRows][nbCols];
-        path = new ArrayList<>();
-        visited = new ArrayList<>();
         monster = new TreeMap<>();
         hunter = new TreeMap<>();
-        tunnel();
-        breakWall(wallPercent);
+        initCells();
+        generatePaths(wallPercent);
     }
 
-    private void breakWall(Integer wallPercent) {
-        for (int i = 0; i < this.wall.length; i++) {
-            for (int j = 0; j < this.wall[0].length; j++) {
-                if (this.wall[i][j] && (random.nextInt(0, 101) > wallPercent)) {
-                    this.wall[i][j] = false;
-
-                }
-            }
-        }
-    }
-
-    private ICoordinate chooseBorderCoord() {
-        int side = random.nextInt(0, 4);
-        int row = 0;
-        int col = 0;
-        switch (side) {
-            case 0:
-                row = 0;
-                col = random.nextInt(0, this.wall[0].length);
-                break;
-            case 1:
-                row = this.wall.length - 1;
-                col = random.nextInt(0, this.wall[0].length);
-                break;
-            case 2:
-                row = random.nextInt(0, this.wall.length);
-                col = 0;
-                break;
-            case 3:
-                row = random.nextInt(0, this.wall.length);
-                col = this.wall[0].length - 1;
-                break;
-        }
-        return new Coordinate(row, col);
-    }
+    /*
+     * private void breakWall(Integer wallPercent) {
+     * for (int i = 0; i < this.wall.length; i++) {
+     * for (int j = 0; j < this.wall[0].length; j++) {
+     * if (this.wall[i][j] && (random.nextInt(0, 101) > wallPercent)) {
+     * this.wall[i][j] = false;
+     * 
+     * }
+     * }
+     * }
+     * }
+     * 
+     * private ICoordinate chooseBorderCoord() {
+     * int side = random.nextInt(0, 4);
+     * int row = 0;
+     * int col = 0;
+     * switch (side) {
+     * case 0:
+     * row = 0;
+     * col = random.nextInt(0, this.wall[0].length);
+     * break;
+     * case 1:
+     * row = this.wall.length - 1;
+     * col = random.nextInt(0, this.wall[0].length);
+     * break;
+     * case 2:
+     * row = random.nextInt(0, this.wall.length);
+     * col = 0;
+     * break;
+     * case 3:
+     * row = random.nextInt(0, this.wall.length);
+     * col = this.wall[0].length - 1;
+     * break;
+     * }
+     * return new Coordinate(row, col);
+     * }
+     * 
+     * public void initCells() {
+     * for (int i = 0; i < this.wall.length; i++) {
+     * for (int j = 0; j < this.wall[0].length; j++) {
+     * this.wall[i][j] = true;
+     * }
+     * }
+     * }
+     * 
+     * public void tunnel() {
+     * initCells();
+     * monster.put(Maze.turn, chooseBorderCoord());
+     * this.wall[monster.get(Maze.turn).getRow()][monster.get(Maze.turn).getCol()] =
+     * false;
+     * path.add(monster.get(Maze.turn));
+     * List<ICoordinate> maxPath = new ArrayList<>();
+     * while (!path.isEmpty()) {
+     * ICoordinate current = path.get(path.size() - 1);
+     * List<ICoordinate> neighbors = getNeighbors(current);
+     * if (!neighbors.isEmpty()) {
+     * ICoordinate next = neighbors.get(random.nextInt(0, neighbors.size()));
+     * path.add(next);
+     * wall[next.getRow()][next.getCol()] = false;
+     * } else {
+     * visited.add(current);
+     * path.remove(current);
+     * }
+     * if (path.size() > maxPath.size()) {
+     * maxPath = new ArrayList<>(path);
+     * }
+     * }
+     * exit = maxPath.get(random.nextInt(maxPath.size() / 2, maxPath.size()));
+     * }
+     * 
+     * public List<ICoordinate> getNeighbors(ICoordinate coord) {
+     * List<ICoordinate> neighbors = new ArrayList<>();
+     * int row = coord.getRow();
+     * int col = coord.getCol();
+     * ICoordinate up = new Coordinate(row - 1, col);
+     * ICoordinate down = new Coordinate(row + 1, col);
+     * ICoordinate left = new Coordinate(row, col - 1);
+     * ICoordinate right = new Coordinate(row, col + 1);
+     * if (row > 0 && nbWall(up) >= 3 && !visited.contains(up)) {
+     * neighbors.add(up);
+     * }
+     * if (row < this.wall.length - 1 && nbWall(down) >= 3 &&
+     * !visited.contains(down)) {
+     * neighbors.add(down);
+     * }
+     * if (col > 0 && nbWall(left) >= 3 && !visited.contains(left)) {
+     * neighbors.add(left);
+     * }
+     * if (col < this.wall[0].length - 1 && nbWall(right) >= 3 &&
+     * !visited.contains(right)) {
+     * neighbors.add(right);
+     * }
+     * return neighbors;
+     * }
+     * 
+     * private int nbWall(ICoordinate up) {
+     * int nb = 0;
+     * int row = up.getRow();
+     * int col = up.getCol();
+     * if (row == 0 || row == this.wall.length - 1 || col == 0 || col ==
+     * this.wall[0].length - 1) {
+     * nb++;
+     * }
+     * if (row > 0 && this.wall[row - 1][col]) {
+     * nb++;
+     * }
+     * if (row < this.wall.length - 1 && this.wall[row + 1][col]) {
+     * nb++;
+     * }
+     * if (col > 0 && this.wall[row][col - 1]) {
+     * nb++;
+     * }
+     * if (col < this.wall[0].length - 1 && this.wall[row][col + 1]) {
+     * nb++;
+     * }
+     * return nb;
+     * }
+     */
 
     public void initCells() {
         for (int i = 0; i < this.wall.length; i++) {
@@ -74,73 +153,112 @@ public class MazeGenerator {
         }
     }
 
-    public void tunnel() {
+    /*
+     * - partir du centre du laby
+     * - calcul le nombre de case a creuser par rapport au % de mur
+     * le diviser par 4
+     * - on va crsuer le laby en 4 partie en partant de chaque direction du centre
+     * dans l'ordre nord-ouest-est sud
+     * - on va cresuer par ligne de taille taillelaby/5 arrondis inferieur a pareil
+     * x 2 (ex laby 11x11 entre 2 et 4)
+     * - sauf la premiere ligne de chaque morceaux qui fait taillelaby/5 arrondis
+     * haut -> taille laby
+     * - on continue a creuser des chemin jusqu'a depassé le nombre requis il faut
+     * donc retenir le nombre de case CREUSER par chemin et un total
+     * - pour choisir la direction on regarde le nombre de voisin on genere un
+     * nombre aléatoire represantant le nombre de direction possible
+     * - et choisir avec tjr l'ordre nord ouest est sud
+     * - exemple si nord et est sont pas dispo on random entre 1 et 2 pour choisir
+     * la direction et l'ordre c'est ouest sud
+     */
+
+    public void generatePaths(Integer wallPercent) {
         initCells();
-        monster.put(Maze.turn, chooseBorderCoord());
-        this.wall[monster.get(Maze.turn).getRow()][monster.get(Maze.turn).getCol()] = false;
-        path.add(monster.get(Maze.turn));
-        List<ICoordinate> maxPath = new ArrayList<>();
-        while (!path.isEmpty()) {
-            ICoordinate current = path.get(path.size() - 1);
-            List<ICoordinate> neighbors = getNeighbors(current);
-            if (!neighbors.isEmpty()) {
-                ICoordinate next = neighbors.get(random.nextInt(0, neighbors.size()));
-                path.add(next);
-                wall[next.getRow()][next.getCol()] = false;
-            } else {
-                visited.add(current);
-                path.remove(current);
-            }
-            if (path.size() > maxPath.size()) {
-                maxPath = new ArrayList<>(path);
-            }
-        }
-        exit = maxPath.get(random.nextInt(maxPath.size() / 2, maxPath.size()));
+        ICoordinate center = new Coordinate(this.wall.length / 2, this.wall[0].length / 2);
+        this.wall[center.getRow()][center.getCol()] = false;
+        int nbEmpty = (this.wall.length * this.wall[0].length * (100 - wallPercent)) / 100;
+        System.out.println(nbEmpty);
+        int nbEmptyByPath = nbEmpty / 4;
+        Stack<ICoordinate> northPath = new Stack<>();
+        northPath.push(center);
+        creuseNord(northPath, nbEmptyByPath);
+        System.out.println(this);
     }
 
-    public List<ICoordinate> getNeighbors(ICoordinate coord) {
-        List<ICoordinate> neighbors = new ArrayList<>();
-        int row = coord.getRow();
-        int col = coord.getCol();
-        ICoordinate up = new Coordinate(row - 1, col);
-        ICoordinate down = new Coordinate(row + 1, col);
-        ICoordinate left = new Coordinate(row, col - 1);
-        ICoordinate right = new Coordinate(row, col + 1);
-        if (row > 0 && nbWall(up) >= 3 && !visited.contains(up)) {
-            neighbors.add(up);
+    private void creuseNord(Stack<ICoordinate> northPath, int nbWallByPath) {
+        Integer firstLineLength = random.nextInt(this.wall.length / 5, this.wall.length / 2);
+        Integer previousDirection = 0;
+        System.out.println(nbWallByPath);
+        System.out.println(firstLineLength);
+        for (int i = 0; i < firstLineLength; i++) {
+            ICoordinate current = northPath.peek();
+            try {
+                ICoordinate next = new Coordinate(current.getRow() - 1, current.getCol());
+                this.wall[next.getRow()][next.getCol()] = false;
+                northPath.push(next);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                break;
+            }
         }
-        if (row < this.wall.length - 1 && nbWall(down) >= 3 && !visited.contains(down)) {
-            neighbors.add(down);
+        nbWallByPath -= firstLineLength;
+        while (nbWallByPath > 0) {
+            System.out.println(nbWallByPath);
+            ICoordinate current = northPath.peek();
+            Integer direction = random.nextInt(0, 4);
+            if (direction == previousDirection) {
+                direction = (direction + 1) % 4;
+            }
+            try {
+                Integer lineLength = random.nextInt(this.wall.length / 5, (this.wall.length / 5) * 2);
+                System.out.println(lineLength);
+                for (int i = 0; i < lineLength; i++) {
+                    ICoordinate next = null;
+                    switch (direction) {
+                        case 0:
+                            next = new Coordinate(current.getRow() - 1, current.getCol());
+                            break;
+                        case 1:
+                            next = new Coordinate(current.getRow(), current.getCol() - 1);
+                            break;
+                        case 2:
+                            next = new Coordinate(current.getRow(), current.getCol() + 1);
+                            break;
+                        case 3:
+                            next = new Coordinate(current.getRow() + 1, current.getCol());
+                            break;
+                    }
+                    if (this.wall[next.getRow()][next.getCol()]) {
+                        this.wall[next.getRow()][next.getCol()] = false;
+                        northPath.push(next);
+                        current = next;
+                        previousDirection = direction;
+                        nbWallByPath--;
+                    }
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+            }
+            System.out.println(this);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        if (col > 0 && nbWall(left) >= 3 && !visited.contains(left)) {
-            neighbors.add(left);
-        }
-        if (col < this.wall[0].length - 1 && nbWall(right) >= 3 && !visited.contains(right)) {
-            neighbors.add(right);
-        }
-        return neighbors;
     }
 
-    private int nbWall(ICoordinate up) {
-        int nb = 0;
-        int row = up.getRow();
-        int col = up.getCol();
-        if (row == 0 || row == this.wall.length - 1 || col == 0 || col == this.wall[0].length - 1) {
-            nb++;
+    public String toString() {
+        String str = "";
+        for (int i = 0; i < this.wall.length; i++) {
+            for (int j = 0; j < this.wall[0].length; j++) {
+                if (this.wall[i][j]) {
+                    str += "X";
+                } else {
+                    str += ".";
+                }
+            }
+            str += "\n";
         }
-        if (row > 0 && this.wall[row - 1][col]) {
-            nb++;
-        }
-        if (row < this.wall.length - 1 && this.wall[row + 1][col]) {
-            nb++;
-        }
-        if (col > 0 && this.wall[row][col - 1]) {
-            nb++;
-        }
-        if (col < this.wall[0].length - 1 && this.wall[row][col + 1]) {
-            nb++;
-        }
-        return nb;
+        return str;
     }
 
     public boolean[][] getWall() {
@@ -157,5 +275,9 @@ public class MazeGenerator {
 
     public ICoordinate getExit() {
         return this.exit;
+    }
+
+    public static void main(String[] args) {
+        MazeGenerator maze = new MazeGenerator(11, 11, 30);
     }
 }
